84ff982bf8a3186e9a0a84c94b498b3130e724e7
PleshakovVV <Pleshakov-V-V@yandex.ru>
2016-07-15 19:00:34 +0300

    Настройка git на Комарова

--- src/classWork/Test.java
+++ src/classWork/Test.java
@@ -4,5 +4,7 @@ package classWork;
  * Created by Student on 15.07.2016.
  */
 public class Test {
-
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
