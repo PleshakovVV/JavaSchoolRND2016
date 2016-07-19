925cc0c6655c9d0b489e08bd3089102aa3c3b454
PleshakovVV <Pleshakov-V-V@yandex.ru>
2016-07-19 22:32:27 +0300

    2051

--- src/homeWork1/_2051/Main.java
+++ src/homeWork1/_2051/Main.java
@@ -1,18 +1,22 @@
 package homeWork1._2051;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Writer;
 import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.util.Stack;
 
 /**
  * Created by Student on 15.07.2016.
  */
 public class Main {
     public static void main(String[] args) throws IOException {
+
         File file = null;
         if (args.length > 0) {
             file = new File(args[0]);
@@ -26,18 +30,25 @@ public class Main {
         }
         long start = System.currentTimeMillis();
         Scanner fileScanner = new Scanner(file);
-        try {
-            String inString = fileScanner.nextLine();
-            fileScanner.close();
+        Stack<Integer> stack = new Stack<>();
+        Writer writer = new FileWriter(file.getParent() + "/output.txt");
+        String inString = fileScanner.nextLine();
+        fileScanner.close();
 
-
-
-            System.out.print("Elapsed time is: ");
-            System.out.print(System.currentTimeMillis() - start);
-            System.out.println(" ms.");
+        for (int i = 0; i < inString.length(); i++) {
+            if (inString.substring(i, i + 1).equals("(")) {
+                stack.push(i + 1);
+            }
+            if (inString.substring(i, i + 1).equals(")")) {
+                writer.write(stack.pop() + " " + (i + 1) + "\n");
+            }
         }
-        catch (InputMismatchException e) {
-            System.out.println("Cannot read Integer value from file.");
-        }
+        writer.flush();
+        writer.close();
+
+        System.out.print("Elapsed time is: ");
+        System.out.print(System.currentTimeMillis() - start);
+        System.out.println(" ms.");
     }
+
 }
