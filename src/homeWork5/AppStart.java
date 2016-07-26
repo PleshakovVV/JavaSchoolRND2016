9b59abcd3170c2eb6b947c5eaac7231a4d9fd615
PleshakovVV <Pleshakov-V-V@yandex.ru>
2016-07-26 14:51:20 +0300

    home work 5

---
+++ src/homeWork5/AppStart.java
@@ -0,0 +1,46 @@
+package homeWork5;
+
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.Scanner;
+
+/**
+ * Created by Master on 26.07.2016.
+ */
+public class AppStart {
+    public static void main(String[] args) {
+        boolean isConnect = false;
+        URL url = null;
+        while (!isConnect) {
+            try {
+                System.out.println("Please, enter URL address to show content(or enter 'exit' for end): ");
+                Scanner scanner = new Scanner(System.in);
+                String address = scanner.nextLine();
+                if (address.trim().toLowerCase().equals("exit")) {
+                    System.out.println("The user enter 'exit'.");
+                    System.exit(0);
+                }
+                url = new URL(address);
+
+                isConnect = true;
+            }
+            catch (MalformedURLException e) {
+                System.out.println("An error occur during trying to find address: " + e.getMessage());
+            }
+
+            try (BufferedReader BR = new BufferedReader(new InputStreamReader(url.openConnection().getInputStream(), Charset.forName("UTF-8")))) {
+                String tempString;
+                while ((tempString = BR.readLine()) != null) {
+                    System.out.println(tempString);
+                }
+                System.out.println("\n---END---");
+            }
+            catch (IOException e) {
+                System.out.println("An error occur during get content: " + e.getMessage());
+                System.out.println("The program end.");
+            }
+        }
+    }
+}
