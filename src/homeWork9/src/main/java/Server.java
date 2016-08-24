33536f7bf30e329883219965434760e69c21b62c
PleshakovVV <Pleshakov-V-V@yandex.ru>
2016-08-24 18:02:48 +0300

    homeWork13 task1 done

---
+++ src/homeWork9/src/main/java/Server.java
@@ -0,0 +1,10 @@
+import java.net.ServerSocket;
+
+/**
+ * Created by Master on 18.08.2016.
+ */
+public class Server {
+    public static void main(String[] args) {
+        ServerSocket serverSocket = new ServerSocket(5151);
+    }
+}
