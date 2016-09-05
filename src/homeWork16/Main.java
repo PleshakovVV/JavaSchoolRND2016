1e7e7770e3f420f30a45eefe42a34941396354b6
Master <Pleshakov-v-v@yandex.ru>
2016-09-05 15:29:06 +0300

    homeWork16 jit

---
+++ src/homeWork16/Main.java
@@ -0,0 +1,16 @@
+package homeWork16;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Master on 03.09.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Map<Integer, String> map = new HashMap<>();
+        for (int i = 0; i < 100000; i++) {
+            map.put(i, "value" + i);
+        }
+    }
+}
