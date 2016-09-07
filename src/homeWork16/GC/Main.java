8cfb77d53854eb2fc6540e218a49fbea6ff161de
PleshakovVV <Pleshakov-V-V@yandex.ru>
2016-09-07 17:06:14 +0300

    homeWork17 1/6

---
+++ src/homeWork16/GC/Main.java
@@ -0,0 +1,10 @@
+package homeWork16.GC;
+
+/**
+ * Created by Master on 05.09.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
--- src/homeWork16/JIT/Main.java
+++ src/homeWork16/JIT/Main.java
@@ -1,4 +1,4 @@
-package homeWork16;
+package homeWork16.JIT;
 
 import java.util.HashMap;
 import java.util.Map;
