841d204f1c6290e8dddbcabd966af57e5891023e
PleshakovVV <Pleshakov-V-V@yandex.ru>
2016-10-13 20:24:05 +0300

    class

--- src/homeWorks/homeWork23/src/main/java/Client.java
+++ src/homeWorks/homeWork23/src/main/java/Client.java
@@ -29,4 +29,12 @@ public class Client implements Serializable{
     public void setName(String name) {
         this.name = name;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
